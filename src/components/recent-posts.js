e5569e85682f7421dfe0f15e030ff186f66ab3ea
Christine Maynard <christinekrm02@gmail.com>
2021-01-14 09:37:06 -0500

    used redux mapStateToProps in the recent posts component

--- src/components/recent-posts.js
+++ src/components/recent-posts.js
@@ -7,20 +7,28 @@ class RecentPosts extends Component {
   componentDidMount() {
     this.props.fetchRecentPosts();
   }
+  renderPosts = function () {
+    const posts = this.props.recentPosts.map((post, index) => {
+      if (index < 2) {
+        return <li key={index}>{post.title}</li>;
+      }
+    });
+    return posts;
+  };
   render() {
     return (
       <div className="recent-posts">
         <div className="recent-posts__wrapper">
           <div className="recent-posts__heading">Recent Posts</div>
-          <ul className="recent-posts__posts">
-            <li>Post 0</li>
-            <li>Post 1</li>
-            <li>Post 2</li>
-          </ul>
+          <ul className="recent-posts__posts">{this.renderPosts()}</ul>
         </div>
       </div>
     );
   }
 }
-
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+  return {
+    recentPosts: state.posts.recentPosts,
+  };
+}
+export default connect(mapStateToProps, actions)(RecentPosts);
