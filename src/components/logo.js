46fb45f4d8539805143cb32701fcc69a9ddbc708
Christine Maynard <christinekrm02@gmail.com>
2021-01-14 10:34:47 -0500

    customized logo component to take in custom size via props. need to fix recent posts searchbar

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,9 +3,14 @@ import React, { Component } from "react";
 
 export default class Logo extends Component {
   render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105,
+    };
     return (
       <div className="logo-main">
         <img
+          style={size}
           src={"/assets/logos/crm.png"}
           height={200}
           width={200}
